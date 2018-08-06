a946c98f8262f4d44a321e3b6e24325f98f82ed4
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-08-06 17:07:37 +0200

    Fix HostKeyCallback

---
+++ simplessh_test.go
@@ -0,0 +1,42 @@
+package simplessh
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/ssh"
+)
+
+func TestMakePrivateKeys(t *testing.T) {
+	assert := assert.New(t)
+	var keyPaths []string
+	var keys []ssh.Signer
+	var err error
+
+	keyPaths = []string{}
+	keys, err = makePrivateKeys(keyPaths)
+	assert.Equal(len(keys), 0)
+	assert.Nil(err)
+
+	keyPaths = []string{"./test/id_rsa"}
+	keys, err = makePrivateKeys(keyPaths)
+	assert.Equal(len(keys), 1)
+	assert.Nil(err)
+}
+
+func TestMakePrivateKey(t *testing.T) {
+	assert := assert.New(t)
+	var keyPath string
+	var key ssh.Signer
+	var err error
+
+	keyPath = "./test/id_rsa"
+	key, err = makePrivateKey(keyPath)
+	assert.NotNil(key)
+	assert.Nil(err)
+
+	keyPath = "./test/id_rsa_404"
+	key, err = makePrivateKey(keyPath)
+	assert.Nil(key)
+	assert.NotNil(err)
+}
